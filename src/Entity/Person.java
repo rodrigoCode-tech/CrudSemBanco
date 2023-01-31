7e562f38a1f0897d84640168e93c511c06be74d0
Rodrigo <rodrigobraga0125@gmail.com>
2023-01-30 21:40:13 -0300

    Projeto Iniciado

---
+++ src/Entity/Person.java
@@ -0,0 +1,58 @@
+package Entity;
+
+public class Person {
+    private int id;
+    private String nome;
+    private String sexo;
+    private String cpf;
+    private String profissao;
+
+    public Person(int id,String nome, String sexo, String cpf, String profissao) {
+        this.id = id;
+        this.nome = nome;
+        this.sexo = sexo;
+        this.cpf = cpf;
+        this.profissao = profissao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+}
+
